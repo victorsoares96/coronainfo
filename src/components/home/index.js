a50909a070e7444ae1a1edd8facb4021dae9109a
Victor Soares <vitorsoares96@hotmail.com>
2020-04-02 22:15:05 -0300

    modified:   src/components/home/index.js

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,64 @@
 import * as React from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
-import { Layout, Text, Button, Card, CardHeader, Avatar } from '@ui-kitten/components';
+import { Layout, Text, Button, Card, CardHeader, Avatar, List, ListItem } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/native';
 
-const Header = () => (<CardHeader title='O que é?'/>);
-const Header2 = () => (<CardHeader title='Sintomas'/>);
+function Definição() {
+  const Header = () => (<CardHeader title='Por que a doença foi batizada de covid-19?'/>);
+  return (
+    <Card style={styles.card} header={Header} status='primary'>
+      <Text>
+        "Co" significa corona, "vi" vem de vírus, e "d" representa 
+        "doença". O número 19 indica o ano de sua aparição, 2019. 
+        Esse nome substitui o de 2019-nCoV, decidido provisoriamente 
+        após o surgimento da doença respiratória. O novo nome foi 
+        escolhido por ser fácil de pronunciar e não ter referência 
+        estigmatizante a um país ou a uma população em particular.
+      </Text>
+    </Card>
+  );
+}
+
+function Surgimento() {
+  const Header = () => (<CardHeader title='Como surgiu o coronavírus?'/>);
+  return (
+    <Card style={styles.card} header={Header} status='primary'>
+      <Text>
+        Novos vírus são descobertos a todo momento. Grande parte pula 
+        de outras espécies, onde passam despercebidos, para os humanos. 
+        A Sars passou para os humanos a partir de um animal selvagem 
+        conhecido como civeta (ou gato-de-algália, parente do guaxinim) 
+        que era considerado uma iguaria na região de Guangdong, na China.
+      </Text>
+    </Card>
+  );
+}
+
+function Transmissão() {
+  const Header = () => (<CardHeader title='Como o coronavírus se transmite?'/>);
+  const data = new Array(8).fill({
+    sintomas: 'Gotículas de saliva',
+    sintomas: 'Espirro',
+    sintomas: 'Tosse',
+    sintomas: 'Catarro',
+    sintomas: 'Contato pessoal próximo (toque ou aperto de mão)',
+    sintomas: 'Contato com objetos ou superfícies contaminadas, seguido de contato com a boca, nariz ou olhos.',
+  });
+  const renderItem = ({ item, index }) => (
+    <ListItem title={`${item.sintomas} ${index + 1}`}/>
+  );
+  return (
+    <Card style={styles.card} header={Header} status='primary'>
+      <Text>
+      A transmissão dos coronavírus costuma ocorrer pelo ar ou por contato pessoal com secreções contaminadas, como:
+      </Text>
+      <List data={data} renderItem={renderItem}/>
+      <Text>
+      O vírus pode ficar incubado por duas semanas, período em que os primeiros sintomas levam para aparecer desde a infecção.
+      </Text>
+    </Card>
+  );
+}
 
 function Home () {
   const navigation = useNavigation();
@@ -13,32 +67,10 @@ function Home () {
       <Layout style={styles.container}>
       <Avatar style={styles.avatar} size='giant' source={require('../../../assets/corona.png')}/>
       <Text style={styles.title}>Corona Vírus</Text>
-      <Card style={styles.card} header={Header} status='primary'>
-        <Text>
-        "Coronavírus" é na verdade o nome dado a um grupo 
-        de vírus pertencentes à mesma família, a Coronaviridae, 
-        que são responsáveis por infecções respiratórias que 
-        podem ser leves ou bastante graves dependendo do 
-        coronavírus responsável pela infecção.{"\n"}{"\n"}
-        
-        Os casos mais graves desta infecção parecem afetar 
-        especialmente pessoas idosas com idade superior a 60 anos, 
-        mas o vírus pode afetar pessoas de todas as idades, sendo, 
-        por isso, muito importante ficar atento ao surgimento de 
-        sintomas que possam indicar a infecção, especialmente febre 
-        alta, tosse persistente e dificuldade para respirar.
-        </Text>
-      </Card>
-  
-      <Card style={styles.card} header={Header2} status='primary'>
-        <Text>
-        Os sintomas da infecção COVID-19 parecem variar de uma simples 
-        gripe até uma infecção mais grave, por isso, se acha que pode 
-        estar infectado, por favor, responda às seguintes perguntas para 
-        saber qual o seu risco:{'\n'}{'\n'}
-        </Text>
-        <Button onPress={() => {navigation.navigate('EXAME');}}>REALIZAR EXAME</Button>
-      </Card>
+      <Definição/>
+      <Surgimento/>
+      <Transmissão/>
+      <Button onPress={() => {navigation.navigate('EXAME');}}>REALIZAR EXAME</Button>
       </Layout>
     </ScrollView>
   );
